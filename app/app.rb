8d9f5814c928bdfbc4bab65f2268b9db085c0d56
jpr <jpr@organisiert.net>
2013-03-31 00:06:08 +0100

    - add register form - add csrf handling

--- app/app.rb
+++ app/app.rb
@@ -7,24 +7,63 @@ require 'bundler/setup'
 # all other dependencies
 require 'sinatra'
 require 'sinatra/reloader'
+require 'mongo'
+#require 'rack/csrf'
 
 use Rack::Session::Pool
-use Rack::Protection
-use Rack::Protection::AuthenticityToken
-use Rack::Protection::FormToken
-use Rack::Protection::RemoteReferrer
+use Rack::Protection, {:use => [:form_token, :remote_referrer]}
 use Rack::Protection::EscapedParams
 
 # setup
 configure do
+	include Mongo
 	@db = MongoClient.new("127.0.0.1", 27017).db('einbeiniger')
-	@users = @db.collection('users');
-	@registrations = @db.collection('registrations')
-	@config = @db.collection('configuration')
+	set :users, @db.collection('users');
+	set :registrations, @db.collection('registrations')
+	set :config, @db.collection('configuration')
 end
 
+helpers do
+	def registration_open?
+		reg_setting = settings.config.find_one('name' => 'registration_open')
+		if (reg_setting.nil?)
+			registration_open = false
+		else
+			registration_open = reg_setting['value']
+		end
+		registration_open
+	end
+
+	def csrf_tag
+		token = random_string
+		session[:csrf] = token
+		"<input type='hidden' name='authenticity_token' value='#{token}' />"
+	end
+
+	def random_string(secure = defined? SecureRandom)
+		secure ? SecureRandom.hex(32) : "%032x" % rand(2**128-1)
+	end
+end
 
 get '/' do
-	@registration_open = @config.find_one('name' => 'registration_open')
+	@registration_open = registration_open?
 	erb :index, :locals => {:registration_open => @registration_open}
+end
+
+get '/register' do
+	# we only accept calls for this, when the registration is really open
+	if (!registration_open?)
+		redirect "/"
+	end
+
+	erb :register
+end
+
+post '/register' do
+	# we only accept calls for this, when the registration is really open
+	if (!registration_open?)
+		redirect "/"
+	end
+
+	erb :register
 end
