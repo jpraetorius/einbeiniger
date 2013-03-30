b4b19cebda4f2c073200e15b0a4debf788271344
jpr <jpr@organisiert.net>
2013-03-30 17:47:08 +0100

    make project build, correct protection in rack

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,19 @@
+# add local classes
 $LOAD_PATH << './lib'
 
+# add bundler for gem handling
+require 'bundler/setup'
+
+# all other dependencies
 require 'sinatra'
+require 'sinatra/reloader'
+
+use Rack::Session::Pool
+use Rack::Protection
+use Rack::Protection::AuthenticityToken
+use Rack::Protection::FormToken
+use Rack::Protection::RemoteReferrer
+use Rack::Protection::EscapedParams
 
 get '/' do
 	erb :index
