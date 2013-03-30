d9cb26bf054b9b3b25b3904d718efea2955e284d
jpr <jpr@organisiert.net>
2013-03-30 19:36:06 +0100

    Add mongodb support and connection

--- app/app.rb
+++ app/app.rb
@@ -15,7 +15,16 @@ use Rack::Protection::FormToken
 use Rack::Protection::RemoteReferrer
 use Rack::Protection::EscapedParams
 
+# setup
+configure do
+	@db = MongoClient.new("127.0.0.1", 27017).db('einbeiniger')
+	@users = @db.collection('users');
+	@registrations = @db.collection('registrations')
+	@config = @db.collection('configuration')
+end
+
+
 get '/' do
-	@registration_open = rand(2) > 0;
+	@registration_open = @config.find_one('name' => 'registration_open')
 	erb :index, :locals => {:registration_open => @registration_open}
 end
