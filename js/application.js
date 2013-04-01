e6e16db9c121b165e5429c1a002d6bae1ebba006
jpr <jpr@organisiert.net>
2013-04-01 15:19:04 +0200

    - prepare deletion of entries

--- js/application.js
+++ js/application.js
@@ -1,9 +1,45 @@
 $(document).ready(function() {
-	$('#registrations-table tr.data').on("click", function(event){
-		if (event.target.tagName != "A") {
-			// show the modal only on non link elements to keep their default behaviour
-			$('#detailsModal').modal('show')
-		}
+	$('#registrations-table i').tooltip();
+
+	$('#registrations-table .details').on("click", function(event){
+		$('#detailsModal').modal('show')
 	});
+
+	$('#registrations-table .delete').on("click", function(event){
+	  var addValue = $(this).hasClass('icon-remove-circle');
+	  $(this).toggleClass('icon-remove-circle icon-trash icon-large text-error')
+	  $(this).closest('tr').toggleClass('error')
+	  var id = $(this).data("id")
+	  var vals = $('#delete-ids').val().split(",");
+	  
+	  if (addValue) {
+	    vals.push(id);
+	    // remove empty element, if there was one
+	    var position = vals.indexOf("");
+	    if (~position){
+	      vals.splice(position, 1);
+	    }
+	  }
+	  else {
+	    var position = vals.indexOf(id);
+	    vals.splice(position, 1);
+	  }
+	  
+	  var newval = "";
+	  if (vals.length == 1) {
+	    newval = vals[0];
+	  }
+	  else if (vals.length > 1) {
+	    newval = vals.join(",");
+	  }
+	  $('#delete-ids').val(newval);
+	  if (vals.length == 0){
+	    $('#delete-button').addClass('disabled').attr('disabled');
+	  }
+	  else {
+	    $('#delete-button').removeClass('disabled').removeAttr('disabled');
+	  }
+	});
+
 	$('#tags').tagsInput();
 });
